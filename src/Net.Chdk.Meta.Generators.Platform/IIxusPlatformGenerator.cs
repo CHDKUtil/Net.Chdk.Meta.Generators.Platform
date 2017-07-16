ed2ea6065dcaa3f51485da4638d39b821ec949eb
Dmitry Shechtman <damnian@gmail.com>
2017-07-16 03:52:33 +0300

    Remove PS platform generators

--- src/Net.Chdk.Meta.Generators.Platform/IIxusPlatformGenerator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Net.Chdk.Meta.Generators.Platform
-{
-    public interface IIxusPlatformGenerator : IPlatformGenerator
-    {
-        string Generate(string source);
-    }
-}
--- src/Net.Chdk.Meta.Generators.Platform/ServiceCollectionExtensions.cs
+++ src/Net.Chdk.Meta.Generators.Platform/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Net.Chdk.Meta.Generators.Platform
 {
@@ -9,30 +8,6 @@ namespace Net.Chdk.Meta.Generators.Platform
         {
             return serviceCollection
                 .AddSingleton<IPlatformGenerator, PlatformGenerator>();
-        }
-
-        [Obsolete]
-        public static IServiceCollection AddIxusPlatformGenerator(this IServiceCollection serviceCollection)
-        {
-            return serviceCollection
-                .AddSingleton<IInnerPlatformGenerator, IxusPlatformGenerator>()
-                .AddSingleton<IIxusPlatformGenerator, IxusPlatformGenerator>();
-        }
-
-        [Obsolete]
-        public static IServiceCollection AddPsEosPlatformGenerator(this IServiceCollection serviceCollection)
-        {
-            return serviceCollection
-                .AddSingleton<IInnerPlatformGenerator, PsEosPlatformGenerator>();
-        }
-
-        public static IServiceCollection AddPsPlatformGenerator(this IServiceCollection serviceCollection)
-        {
-            return serviceCollection
-                .AddSingleton<IInnerPlatformGenerator, PowerShotPlatformGenerator>()
-                .AddSingleton<IInnerPlatformGenerator, IxusPlatformGenerator>()
-                .AddSingleton<IIxusPlatformGenerator, IxusPlatformGenerator>()
-                .AddSingleton<IInnerPlatformGenerator, PsEosPlatformGenerator>();
         }
     }
 }
